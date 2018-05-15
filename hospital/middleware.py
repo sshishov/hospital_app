b914bac4c5dd8b06257f46704839278a1e247299
Sergey Shishov <sshishov.sshishov@gmail.com>
2018-05-16 02:44:01 +0400

    Fix middleware misprint

--- hospital/middleware.py
+++ hospital/middleware.py
@@ -3,7 +3,7 @@ class ProjectMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        if not request.session.get('project'):
+        if not request.session.get('project') and request.user.is_authenticated and hasattr(request.user, 'userprofile'):
             project = request.user.userprofile.projects.first()
             if project:
                 request.session['project'] = str(project.id)
