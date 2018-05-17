8174493000220d78a3e95234e9e41626e28bb008
Sergey Shishov <sshishov.sshishov@gmail.com>
2018-05-17 10:45:22 +0400

    Update production settings

--- hospital/conf/production_settings.py
+++ hospital/conf/production_settings.py
@@ -1,13 +1,14 @@
 import os
 
 # Django standard configuration
-os.environ['H_DJANGO_ALLOWED_HOSTS'] = os.environ.get('H_DJANGO_ALLOWED_HOSTS', '.hospitalapp.com')
+os.environ['H_DJANGO_ALLOWED_HOSTS'] = os.environ.get('H_DJANGO_ALLOWED_HOSTS', '.1medsys.com')
 os.environ['H_DJANGO_ADMIN_URL'] = os.environ.get('H_DJANGO_ADMIN_URL', 'hospital_admin')
+os.environ['H_VERIFY_SSL'] = os.environ.get('H_VERIFY_SSL', 'false')
 
 # Redis
-os.environ['H_REDIS'] = os.environ.get('H_REDIS', 'redis://<redis_url>:6379/')
+os.environ['H_REDIS'] = os.environ.get('H_REDIS', 'redis://redis.1medsys.com:6379/')
 
 # Logging
-os.environ['LOGSTASH_HOST'] = os.environ.get('LOGSTASH_HOST', 'logman.dubizzlecloud.com')
+os.environ['LOGSTASH_HOST'] = os.environ.get('LOGSTASH_HOST', 'logstash.1medsys.com')
 
 from hospital.conf.base_settings import *  # noqa: E501,F403  # pylint:disable=wildcard-import,wrong-import-position
