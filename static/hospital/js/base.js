ba4e2baf0ca7919fea8206df7da1aa557e7078df
Sergey Shishov <sshishov.sshishov@gmail.com>
2018-06-16 21:51:59 +0400

    Add proper ordering for datatable (by date modified)

--- static/hospital/js/base.js
+++ static/hospital/js/base.js
@@ -1,6 +1,7 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
   $('#patientHistoryTable').DataTable({
+    'order': [[ $("#patientHistoryTable > tbody > tr:first > td").length - 1, "desc" ]],
     'language': {
       'url': "//cdn.datatables.net/plug-ins/1.10.16/i18n/Russian.json"
     },
